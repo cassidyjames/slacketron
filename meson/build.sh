868570ee3ebd26fb3eae024ddcfef5144f623ea4
Cassidy James Blaede <c@ssidyjam.es>
2020-08-30 17:09:20 -0600

    Use tray mode to support running in background

--- meson/build.sh
+++ meson/build.sh
@@ -4,7 +4,8 @@ nativefier https://app.slack.com/client/ \
   --name Slack \
   --background-color '#ededed' \
   --single-instance \
-  --disable-dev-tools
+  --disable-dev-tools \
+  --tray
 
 cp -r Slack-linux-arm64 "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/"
 
